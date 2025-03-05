6459e958c26660a812890f7d3d262b2e4fa2ce24
Van Cameron Dia <vdia@academic.rrc.ca>
2025-03-04 18:11:20 -0600

    Added a simple floating action button

--- app/src/main/java/com/example/coopt1/MainActivity.kt
+++ app/src/main/java/com/example/coopt1/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.coopt1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.coopt1.ui.theme.CoOpt1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +31,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoOpt1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    FAB()
+
                 }
             }
         }
@@ -31,17 +40,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FAB() {
+    val context = LocalContext.current
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        FloatingActionButton(
+            onClick = {Toast.makeText(context, "FAB Clicked", Toast.LENGTH_SHORT).show()} ,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomEnd),
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.secondary
+        ) {
+            Icon(Icons.Filled.Add, "Floating action button.")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview(){
     CoOpt1Theme {
-        Greeting("Android")
+        Example()
     }
 }
